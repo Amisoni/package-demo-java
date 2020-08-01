5420e56409971fe3644111c0bfc947e1d865c045
Ami Soni <55349992+Amisoni@users.noreply.github.com>
2020-08-02 01:34:14 +0530

    Add files via upload

---
+++ cal.java
@@ -0,0 +1,16 @@
+import temp.*;
+class cal
+{
+	public static void main(String args[])
+	{
+		demo a=new demo();
+		a.sub(10,5);
+
+
+		a.sum(30,40);
+
+
+		a.mul(5,2);
+		a.div(10,2);
+	}
+}
---
+++ temp/demo.java
@@ -0,0 +1,20 @@
+package temp;
+public class demo
+{
+	public void sub(int x,int y)
+	{
+		System.out.println("Sub Is:"+(x-y));
+	}
+	public void sum(int x,int y)
+	{
+		System.out.println("Sum Is:"+(x+y));
+	}
+	public void mul(int x, int y)
+	{
+		System.out.println("Mul Is:"+(x*y+200));
+	}
+	public void div(int x,int y)
+	{
+		System.out.println("DIv Is:"+(x/y));	
+	}
+}
